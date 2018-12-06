fd837afc97a25db215c99b46e0389fb11b4e21d7
Péter <milanovitsp@gmail.com>
2018-12-06 16:26:50 +0100

    eod

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -28,22 +28,20 @@ export default class Modal extends Component {
   render() {
     const item = this.props.item;
     return ReactDOM.createPortal(
-      <div onClick={this.handleClick} className="modal-bkg">
+      <div onClick={this.handleClick} className="modal-background">
         <div className="modal" ref={node => this.node = node} >
-          <div>
-            <div>
-              {
-                item.img_alt.length > 0 ? <ImageCarousel item={item} /> : <SingleImage item={item} />
-              }
-            </div>
-            <div>
-              <div>{item.name}</div>
-              <div>{item.ln}</div>
-            </div>
+          <div className="modal-left">
+            {
+              item.img_alt.length > 0 ? <ImageCarousel item={item} /> : <SingleImage item={item} />
+            }
           </div>
-          <hr />
-          <button onClick={() => this.handleOrder(item.ln, this.state.itemAmount)}>Megrendel</button>
-          <button onClick={this.props.onClose}>Bezár</button>
+          <div className="modal-right">
+            <div className="modal-right__close-x" onClick={this.props.onClose}></div>
+            <div className="modal-right__product-name">{item.name}</div>
+            <div className="modal-right__product-ln">{item.ln}</div>
+            <div className="modal-right__product-description">{item.description}</div>
+            <button onClick={() => this.handleOrder(item.ln, this.state.itemAmount)}>Megrendel</button>
+          </div>
         </div>
       </div>,
       this.modalRoot,
@@ -52,14 +50,32 @@ export default class Modal extends Component {
 }
 
 class ImageCarousel extends Component {
+  state = {
+    mainImg: "",
+  }
+
+  // Set default main image to first of secondary images
+  componentDidMount = () => {
+    this.setState({
+      mainImg: this.props.item.img_alt[0]
+    })
+  }
+
+  // Change main image src to clicked one
+  handleClick = (e) => {
+    this.setState({
+      mainImg: e.target.src
+    })
+  }
+
   render() {
     const img_alt = this.props.item.img_alt
     return (
-      <div className="modal-carousel">
-        <img src={img_alt[0]} alt="MainProduct" className="modal-carousel__main" />
-        <div className="modal-carousel__container">
-          {img_alt.map(function (img, i) {
-            return <img key={i} src={img} alt="AltProduct" className="modal-carousel__carouselImg" />
+      <div className="modal-left__carousel">
+        <img src={this.state.mainImg} alt="MainProduct" className="modal-left__main-image" />
+        <div className="modal-left__carousel__secondary-container">
+          {img_alt.map((img, i) => {
+            return <img key={i} src={img} onClick={this.handleClick} alt="AltProduct" className="modal-left__carousel__secondary-image" />
           })}
         </div>
       </div>
@@ -71,7 +87,7 @@ class SingleImage extends Component {
   render() {
     const img = this.props.item.img
     return (
-      <img src={img} alt="SingleImage" className="modal-singleimage" />
+      <img src={img} alt="SingleImage" className="modal-left__main-image" />
     )
   }
 }
