04bcba4b5fa7b7f537bf6f19e55f2b522bf54779
Péter <milanovitsp@gmail.com>
2018-12-17 16:17:49 +0100

    eod

--- src/PointCollection.js
+++ src/PointCollection.js
@@ -9,49 +9,76 @@ class PointCollection extends Component {
     this.state = {
       aws: 19000,
       target: 23000,
+      displayPercent: 0,
+      displayAws: 0,
+      displayBrochures: 0,
     }
   }
 
   componentDidMount = () => {
-    this.startCircleCounter();
+    this.startCounters();
   }
 
-  startCircleCounter = () => {
-    const percent = this.state.aws + 0.3;
+  startCounters = () => {
+    const percent = this.state.displayPercent + (this.state.aws / this.state.target) * 0.2;
+    const maxallowed = this.state.aws / this.state.target * 100;
+
     if (percent >= 100) {
-      this.setState({ percent: 105 }); // Closing the visual gap between the ends.
+      this.setState({ displayPercent: 105 }); // Closing the visual gap between the ends.
+      this.setState({ displayAws: this.state.aws })
       clearTimeout(this.timeout);
       return;
     }
-    this.setState({ percent });
-    this.timeout = setTimeout(this.startCircleCounter, 1);
+    if (percent <= maxallowed) {
+      this.setState({ displayPercent: percent })
+      this.setState({ displayAws: Math.round((this.state.aws * percent) * 0.01) })
+    }
+    this.timeout = setTimeout(this.startCounters, 1);
   }
 
   render() {
     return (
-      <div>
-        <h1>Point Collection</h1>
+      <div className="pointCollection">
+        <div className="datatable">
+          <div className="datatable--col">
+            <div className="datatable--col__item">
+              <div className="datatable--col__item__title">Minimum katalógusok</div>
+              <div className="datatable--col__item__content">1 db</div>
+            </div>
+            <div className="datatable--col__item">
+              <div className="datatable--col__item__title">Megrendelt katalógusok</div>
+              <div className="datatable--col__item__content">1 db</div>
+            </div>
+          </div>
+          <div className="datatable--col">
+            <div className="datatable--col__item">
+              <div className="datatable--col__item__title">Megszerzett pontjaid</div>
+              <div className="datatable--col__item__content">100</div>
+            </div>
+            <div className="datatable--col__item">
+              <div className="datatable--col__item__title">Szerezhető alappontok</div>
+              <div className="datatable--col__item__content">10</div>
+            </div>
+          </div>
+        </div>
+
         <div className="infograf">
-          <div className="infograf__row">
-            <div className="infograf__item">
-              <h3>Target: 19 000Ft</h3>
-            </div>
-            <div className="infograf__item">
-              <h3>Brosi: min 1 </h3>
-            </div>
+          <div className="infograf--col__left">
+            <div className="aws">AWS: {this.state.displayAws}</div>
+            <Circle percent={this.state.displayPercent} strokeWidth="10" strokeColor="#888" trailWidth="4" trailColor="#ccc" />
           </div>
-          <div className="infograf__row">
-            <div className="infograf__item">
-              <h3>Base points: 10pt</h3>
+          <div className="infograf--col__right">
+            <div>
+              <div>Célkitűzésed</div>
+              <div>123 456 Ft</div>
             </div>
-            <div className="infograf__item">
-              <h3>Brosi bought: 10db</h3>
+            <div>
+              <div>Eddigi összmegrendelésed</div>
+              <div>10 000 Ft</div>
             </div>
-          </div>
-          <div className="infograf__row">
-            <div className="infograf__item">
-              <h3>Target {this.state.target} Ft</h3>
-              <Circle percent={this.state.aws} strokeWidth="8" strokeColor="#888" trailWidth="4" trailColor="#ccc" />
+            <div>
+              <div>Még ennyiért rendelj a teljesítéshez</div>
+              <div>50 000 Ft</div>
             </div>
           </div>
         </div>
